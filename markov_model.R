4f375e88423ea0ce53c1f62155a8907ced7c22e5
apsicle <rcyan@email.wm.edu>
2015-07-10 14:26:47 -0400

    Added risk curves, changed output selection for runModel()

--- markov_model.R
+++ markov_model.R
@@ -1,6 +1,8 @@
 #markov_model.R
 #Model for invasive species spread. Uses a state vector of decimals representing the "fractional
 #establishment" of an invasive species in a certain region.
+library(ggplot2)
+library(reshape2)
 
 #Load shipping data (trucks, logs)
 d1<-read.csv("1997FAF_all_trucks_comb.csv",header=TRUE)
@@ -36,37 +38,52 @@ states <- c("Alabama","Alaska","Arizona","Arkansas","California",
 runModel <- function(S_0, N, m1, m2, m3, m4, names = states, out = "state") {
     S_n <- S_0
     len = length(names)
-    if(out == "state") {
-        output1 = list(0)
+    if(out == "proportion" || out == "both") {
+        out_proportions = list(0)
     }
-    #else if(out == "establishment") {
-    #    output = data.frame(state = names, invaded = 
-    #}
-    if(out == "state") {
-        output = list(0)
+    if(out == "state" || out == "both") {
+        out_state = list(0)
     }
     
     for(n in 1:N) {
         indices <- which(S_n >= 1)
         P <- generateP(m1, m2, m3, m4)$P
         
-        if(out == "state") {
+        if(out == "proportion" || out == "both") {
             proportions = matrix(nrow = len, ncol = len)
             for(j in 1:len) {
                 proportions[, j] <- P[, j]*S_n[j]
+                proportions[, j] <- proportions[, j] / sum(proportions[, j])
             }
-            output1[[n]] <- proportions
+            out_proportions[[n]] <- proportions
         }
-        
         
         S_n <- P%*%S_n
         S_n[indices] <- 1
         
-        if(out == "state") {
-            output[[n]] <- S_n
+        if(out == "state" || out == "both") {
+            out_state[[n]] <- S_n
         }
     }
-    out <- list(output1, output)
+    if(out == "proportion") {
+        out = out_proportions
+    }
+    else if(out == "state") {
+        out = out_state
+    }
+    else if(out == "both") {
+        out = list(proportions = out_proportions, state = out_state)
+    }
+    
+    out
+}
+
+runMany <- function(init, times, steps, m1, m2, m3, m4, names = states, method = "state") {
+    out = list(0)
+    for(i in 1:times) {
+        out[[i]] <- runModel(S_0 = init, N = steps, m1 = m1, m2 = m2, m3 = m3, m4 = m4,
+                             names = names, out = method)
+    }
     out
 }
 
@@ -79,9 +96,46 @@ generateStats <- function(out) {
     total
 }
 
-generateRisk <- function(state, time, levels = c(0.1, 0.2, 0.5)) {
-    totals <- list(0)
-    for(i in 1:100) {
-        totals[[i]] <- runModel()
+generateRisk <- function(data, state, levels = c(0.1, 0.2, 0.5)) {
+    
+    state_code <- which(states == state)
+    time_steps <- length(data[[1]])
+    num_levels <- length(levels)
+    num_runs <- length(data)
+    output_data <- list(0)
+    
+    for(i in 1:num_levels) {
+        
+        level = levels[i]
+        prob <- numeric(time_steps)
+        
+        for(step in 1:time_steps) {
+            
+            count = 0
+            
+            for(run in 1:num_runs) {
+                current_run <- data[[run]][[step]][state_code]
+                if(current_run > level) {
+                    count = count + 1
+                }
+            }
+            
+            prob[step] <- count / num_runs
+        }
+        df <- data.frame(probability = prob, time = 1:time_steps, level = as.factor(level),
+                         state = state)
+        output_data[[i]] <- df
     }
+    output_data
+}
+
+plotRisk <- function(risk_data) {
+    levels = length(risk_data)
+    df <- do.call("rbind", risk_data)
+    state = levels(df$state)
+    title <- paste("Invasion Risk Over Time for", state)
+    plot <- ggplot(df, aes(time, probability, col = level))
+    plot <- plot + geom_smooth() + ggtitle(title)
+    plot <- plot + xlab("Years") + ylab("Probability[Establishment > Level]")
+    plot
 }
